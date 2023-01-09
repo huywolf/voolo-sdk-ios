9a3767b73ccc63c99a257ebe78052ddd52b3804e
huywolfsolutions <hoanghuy.le@wolfsolutions.vn>
2023-01-09 13:37:37 +0700

    add framework

---
+++ VooloSDK.xcframework/ios-arm64/VooloSDK.framework/Headers/VooloSDK.h
@@ -0,0 +1,18 @@
+//
+//  VooloSDK.h
+//  VooloSDK
+//
+//  Created by Tuan Tang on 04/04/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VooloSDK.
+FOUNDATION_EXPORT double VooloSDKVersionNumber;
+
+//! Project version string for VooloSDK.
+FOUNDATION_EXPORT const unsigned char VooloSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VooloSDK/PublicHeader.h>
+
+
